f04a978239e4c3873f8d967c85cbc146ef49ea49
FKSHyper <128801212+FKSHyper@users.noreply.github.com>
2026-03-25 20:12:37 +0000

    Form1 - Feito Estat - Criado e Visualmente feito

--- Empresa/Form1.Designer.cs
+++ Empresa/Form1.Designer.cs
@@ -28,18 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnRegisto = new Button();
+            btnDetalhes = new Button();
+            btnEstat = new Button();
             SuspendLayout();
+            // 
+            // btnRegisto
+            // 
+            btnRegisto.Location = new Point(12, 12);
+            btnRegisto.Name = "btnRegisto";
+            btnRegisto.Size = new Size(112, 73);
+            btnRegisto.TabIndex = 0;
+            btnRegisto.Text = "Registo";
+            btnRegisto.UseVisualStyleBackColor = true;
+            btnRegisto.Click += btnRegisto_Click;
+            // 
+            // btnDetalhes
+            // 
+            btnDetalhes.Location = new Point(130, 12);
+            btnDetalhes.Name = "btnDetalhes";
+            btnDetalhes.Size = new Size(112, 73);
+            btnDetalhes.TabIndex = 1;
+            btnDetalhes.Text = "Detalhes";
+            btnDetalhes.UseVisualStyleBackColor = true;
+            btnDetalhes.Click += btnDetalhes_Click;
+            // 
+            // btnEstat
+            // 
+            btnEstat.Location = new Point(248, 12);
+            btnEstat.Name = "btnEstat";
+            btnEstat.Size = new Size(112, 73);
+            btnEstat.TabIndex = 2;
+            btnEstat.Text = "Estatisticas";
+            btnEstat.UseVisualStyleBackColor = true;
+            btnEstat.Click += btnEstat_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(444, 450);
+            Controls.Add(btnEstat);
+            Controls.Add(btnDetalhes);
+            Controls.Add(btnRegisto);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnRegisto;
+        private Button btnDetalhes;
+        private Button btnEstat;
     }
 }
--- Empresa/Form1.cs
+++ Empresa/Form1.cs
@@ -6,5 +6,26 @@ namespace Empresa
         {
             InitializeComponent();
         }
+
+        private void btnRegisto_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Registo registo = new Registo();
+            registo.Show();
+        }
+
+        private void btnDetalhes_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Detalhes detalhes = new Detalhes();
+            detalhes.Show();
+        }
+
+        private void btnEstat_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Estatisticas estat = new Estatisticas();
+            estat.Show();
+        }
     }
 }
